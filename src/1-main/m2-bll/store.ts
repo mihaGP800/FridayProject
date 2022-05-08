4f09911251aa0bfe9feefd2fa3883ab0217232ba
mihaGP800 <mihagoroh@gmail.com>
2022-05-08 11:29:45 +0300

    HW14

--- src/1-main/m2-bll/store.ts
+++ src/1-main/m2-bll/store.ts
@@ -12,7 +12,6 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 //all types actions for app
 export type AppActionsType = ProfileActionsType | any
 
-
 //AppThunk type
 export type AppThunk<ReturnType = void> =
     ThunkAction<ReturnType, AppRootStateType, unknown, AppActionsType>
